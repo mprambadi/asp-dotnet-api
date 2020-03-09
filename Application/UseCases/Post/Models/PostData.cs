46e8bbfcbc5a019978a3a2f091d19a9c59a41843
Maulana <mprambadi@gmail.com>
2020-03-09 13:18:54 +0700

    add creator services

--- Application/UseCases/Post/Models/PostData.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_mediatr.Application.UseCases.Post.Models
-{
-    public class PostData
-    {
-        public string Title { get; set; }
-        public string Body { get; set; }
-    }
-}
--- Application/UseCases/Post/Queries/GetPost/GetPostQuery.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-
-namespace dotnet_mediatr.Application.UseCases.Post.Queries.GetPost
-{
-    public class GetPostQuery: IRequest<GetPostDto>
-    {
-        public int id { get; set; }
-    }
-}
